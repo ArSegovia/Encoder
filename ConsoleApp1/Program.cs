73e82a1c0cbc906e7b46cfe3626e66c1c04d1ad9
ArSegovia <105670389+ArSegovia@users.noreply.github.com>
2022-06-01 14:21:44 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿string? texto = Console.ReadLine();
+
+if (texto != null)
+{   
+    texto = String.Concat(texto.Where(c => !Char.IsWhiteSpace(c)));
+    int len = texto.Length;
+    double raiz = Math.Sqrt(len); 
+    double piso = Math.Floor(raiz);
+    double techo = Math.Ceiling(raiz);
+    int num = 0;
+
+    for (int i = (int)piso; i <= (int)techo; i++)
+    {
+        if (i * i >= len)
+        {
+            num = i;
+            break;
+        }
+    }
+
+    string[] tabla1 = texto
+    .Chunk(num)
+    .Select(x => new string(x))
+    .ToArray();
+
+    for (int i = 0; i < num; i++)
+    {
+        foreach (string s in tabla1)
+        {
+            int cant = s.Length;
+            if (i < cant)                
+                Console.Write(s[i]);
+        }
+        Console.Write(" ");
+    }   
+}
+
+
+
+
